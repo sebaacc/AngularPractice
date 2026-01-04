8b634062a8aca3eccdeb5766e154a70f81fde76a
Sebastián-Markoja <sebaacc11@gmail.com>
2026-01-03 21:47:00 -0300

    segundo commit clase 9

--- src/app/pages/counter/counter-page.ts
+++ src/app/pages/counter/counter-page.ts
@@ -1,15 +1,27 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 @Component({
   selector: 'app-counter-page',
   templateUrl: './counter-page.html',
-  styleUrls: ['./counter-page.css']
+  styleUrls: ['./counter-page.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush ---> no hace falta en la versión actual para que funcione el update dentro del interval... de la línea 14.
 })
 export class CounterPageComponent {
   counter: number = 10;
+  CounterSignal = signal(10);
+
+  // constructor() {
+  //   setInterval(() => {
+  //     this.CounterSignal.update(v => v + 1)
+  //     console.log('Tick');
+  //   }, 2000);
+  // }
+
   increaseBy(value: number) {
     this.counter += value;
+    this.CounterSignal.update(current => current + value);
   }
   reset(){
-    this.counter = 10;
+    this.counter = 0;
+    this.CounterSignal.set(0);
   }
 }
